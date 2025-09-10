09bfc03e4cf96bea92dbcbc4375a8aa7ceaec588
KauaBasagliaSoares <kauabasaglia@gmail.com>
2025-09-10 20:00:27 -0300

    Add files via upload

---
+++ Atividades lab.prog SI/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main(){
+
+        int anos = 0;
+
+        printf("Digite a sua idade: \n");
+        scanf("%d", &anos);
+        printf("Voce tem %d anos ", anos);
+
+                return 0;
+    }
